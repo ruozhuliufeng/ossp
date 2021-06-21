77c5a320be106b43f6728ef8bfe3abb4e26a8b1d
ruozhuliufeng <18852971173@163.com>
2021-06-21 12:41:57 +0800

    添加说明文档，以及项目基础结构

---
+++ ossp-common/src/main/java/cn/aixuxi/ossp/common/OsspCommonApplication.java
@@ -0,0 +1,13 @@
+package cn.aixuxi.ossp.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsspCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsspCommonApplication.class, args);
+    }
+
+}
---
+++ ossp-gateway/src/main/java/cn/aixuxi/ossp/gateway/OsspGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.aixuxi.ossp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsspGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsspGatewayApplication.class, args);
+    }
+
+}
